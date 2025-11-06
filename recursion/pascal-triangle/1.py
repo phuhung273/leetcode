bb1d4df4a1ac33debfd192a702365f00475a6592
phuhung273 <phuhung.tranpham@gmail.com>
2025-11-06 07:11:26 +0700

    pascals-triangle recursion
    Signed-off-by: phuhung273 <phuhung.tranpham@gmail.com>

---
+++ recursion/pascal-triangle/1.py
@@ -0,0 +1,31 @@
+"""
+Idea: Recursion
+
+Get result of f(n-1)
+
+Calculate row[n] from row[n-1]
+
+Time: O(N^2) = 1 + 2 + 3 + ... + n
+
+Space: O(N^2) = height of Recursion stack N * nums of local variable (numRows 1 + result N)
+"""
+
+from typing import List
+
+
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        if numRows == 1:
+            return [[1]]
+
+        result = self.generate(numRows - 1)
+        newRow = [1]
+        lastRow = result[len(result) - 1]
+        for i in range(1, len(lastRow)):
+            newRow.append(lastRow[i - 1] + lastRow[i])
+        newRow.append(1)
+        result.append(newRow)
+        return result
+
+sol = Solution()
+sol.generate(5)
