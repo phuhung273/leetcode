a027bd463c3b463f19fb31b8b3aa776d274e9453
phuhung273 <phuhung.tranpham@gmail.com>
2025-11-07 11:30:30 +0700

    combinations
    Signed-off-by: phuhung273 <phuhung.tranpham@gmail.com>

---
+++ backtracking/combinations/1.py
@@ -0,0 +1,29 @@
+"""
+Time: O(N^k)
+Space: O(N^k) = N^k(ans) + k(depth of recursion stack) * 1(input i)
+"""
+
+from typing import List
+
+
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        ans = []
+        current = []
+
+        def choose(i: int):
+            if len(current) == k:
+                ans.append(current.copy())
+                return
+
+            for j in range(i, n + 1):
+                current.append(j)
+                choose(j + 1)
+                current.pop()
+
+        choose(1)
+        return ans
+
+sol = Solution()
+sol.combine(n=4, k=2)
+sol.combine(n=1, k=1)
