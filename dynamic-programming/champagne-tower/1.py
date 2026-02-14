9503d9b4a597516e9c51534d6a6143868fff4af4
phuhung273 <phuhung.tranpham@gmail.com>
2026-02-14 14:19:37 +0700

    champagne-tower bottom up
    Signed-off-by: phuhung273 <phuhung.tranpham@gmail.com>

--- dynamic-programming/champagne-tower/1.py
+++ dynamic-programming/champagne-tower/1.py
@@ -20,38 +20,59 @@ from functools import cache
 
 
 class Solution:
-    # Top down
-    def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
-        if poured == 0:
-            return 0
-        elif poured == 1:
-            return 1 if query_row == 0 else 0
+    # # Top down
+    # def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
+    #     if poured == 0:
+    #         return 0
+    #     elif poured == 1:
+    #         return 1 if query_row == 0 else 0
 
-        @cache
-        def pour(query_row: int, query_glass: int) -> float:
-            nonlocal poured
+    #     @cache
+    #     def pour(query_row: int, query_glass: int) -> float:
+    #         nonlocal poured
 
-            # Base case
-            if query_row == 0:
-                return poured
+    #         # Base case
+    #         if query_row == 0:
+    #             return poured
 
-            amount = 0
-            if query_glass != query_row:
-                upper = pour(query_row - 1, query_glass)
-                if upper > 1:
-                    amount += (upper - 1) / 2
-            if query_glass != 0:
-                upper = pour(query_row - 1, query_glass - 1)
-                if upper > 1:
-                    amount += (upper - 1) / 2
-            return amount
+    #         amount = 0
+    #         if query_glass != query_row:
+    #             upper = pour(query_row - 1, query_glass)
+    #             if upper > 1:
+    #                 amount += (upper - 1) / 2
+    #         if query_glass != 0:
+    #             upper = pour(query_row - 1, query_glass - 1)
+    #             if upper > 1:
+    #                 amount += (upper - 1) / 2
+    #         return amount
         
-        ans = pour(query_row, query_glass)
-        if ans > 1:
-            return 1
-        elif ans < 0:
-            return 0
-        return ans
+    #     ans = pour(query_row, query_glass)
+    #     if ans > 1:
+    #         return 1
+    #     elif ans < 0:
+    #         return 0
+    #     return ans
+    
+    # Bottom up
+    def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
+        lastDp = [0] * (query_row + 1)
+        lastDp[0] = poured
+        for row in range(1, query_row + 1):
+            isEnough = False
+            dp = [0.0] * (query_row + 1)
+            for col in range(row + 1):
+                if col < row:
+                    if lastDp[col] > 1:
+                        isEnough = True
+                        dp[col] += (lastDp[col] - 1) / 2
+                if col > 0:
+                    if lastDp[col - 1] > 1:
+                        isEnough = True
+                        dp[col] += (lastDp[col - 1] - 1) / 2
+            if not isEnough:
+                return 0
+            lastDp = dp
+        return lastDp[query_glass] if lastDp[query_glass] < 1 else 1
 
 
 sol = Solution()
