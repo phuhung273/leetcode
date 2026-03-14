6d1c4a67c0fc8a4a1da93b93d7e35b59aaa50e70
phuhung273 <phuhung.tranpham@gmail.com>
2026-03-14 16:49:01 +0700

    simplify reorder-routes-to-make-all-paths-lead-to-the-city-zero
    Signed-off-by: phuhung273 <phuhung.tranpham@gmail.com>

--- graph/reorder-routes-to-make-all-paths-lead-to-the-city-zero/1.py
+++ graph/reorder-routes-to-make-all-paths-lead-to-the-city-zero/1.py
@@ -19,22 +19,16 @@ from typing import List
 class Solution:
     def minReorder(self, n: int, connections: List[List[int]]) -> int:
         ans = 0
-        q = deque()
         toGraph = defaultdict(set) # to key, we have
         fromGraph = defaultdict(set) # from key, we have
 
         for u, v in connections:
             toGraph[v].add(u)
             fromGraph[u].add(v)
-        
-        for i in toGraph[0]:
-            q.append(i)
-        for i in fromGraph[0]:
-            ans += 1
-            q.append(i)
 
-        done = set([0])
-        
+        done = set()
+        q = deque([0])
+
         while q:
             i = q.popleft()
             for u in toGraph[i]:
