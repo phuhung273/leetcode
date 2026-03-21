7ed7dc116f192ad380120ed3791b84158a9eafa8
phuhung273 <phuhung.tranpham@gmail.com>
2026-03-21 10:31:11 +0700

    rotate-image
    Signed-off-by: phuhung273 <phuhung.tranpham@gmail.com>

---
+++ matrix/rotate-image/1.py
@@ -0,0 +1,42 @@
+"""
+Problem: https://leetcode.com/problems/rotate-image
+Idea:
+Flip matrix vertically
+1   2   3
+4   5   6
+7   8   9
+
+7   8   9
+4   5   6
+1   2   3
+
+Swap matrix[y][x] <> matrix[x][y]
+7   4   1
+8   5   2
+9   6   3
+
+Time: O(n^2)
+Space: O(1)
+"""
+
+from typing import List
+
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        for i in range(n // 2):
+            matrix[i], matrix[n - 1 - i] = matrix[n - 1 - i], matrix[i]
+        
+        for i in range(n):
+            for j in range(i + 1, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        return
+
+
+sol = Solution()
+sol.rotate([[1,2,3],[4,5,6],[7,8,9]])
+sol.rotate([[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]])
